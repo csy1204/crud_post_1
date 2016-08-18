ee4a6f761253ee8591e237d6e2374a05fbab61ad
csy1204 <josang1204@gmail.com>
2016-08-18 08:04:05 +0000

    second

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,37 @@
 class HomeController < ApplicationController
+
   def index
     @every_post = Post.all
 
   end
   
   def write
-    post = Post.new(title: params[:title], content: params[:content])
+
+    post = Post.new(title: params[:title], content: params[:content], user: current_user)
+    authorize! :write, Post
     post.save
+    
+    
     
     redirect_to "/home/index"
 
   end
 
   def update
+    
     @one_post = Post.find(params[:id])
+    authorize! :update, @one_post
+
+    
+    
   end
   
   def destroy
     @one_post = Post.find(params[:id])
+    authorize! :destroy, @one_post
     @one_post.destroy
     redirect_to '/home/index'
+
 
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'home#index'
   get 'home/index'
   post '/home/write' => 'home#write'
   get 'destroy/:id' => 'home#destroy'
